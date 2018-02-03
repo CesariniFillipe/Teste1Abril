355f03f3d4547060ce0a1993a679684f08f5ae25
Fillipe Cesarini <fillipe_cesarini_07@hotmail.com>
2018-02-03 18:53:26 -0200

    Remoção do dist

--- inc/header.php
+++ inc/header.php
@@ -6,14 +6,14 @@
 	<title>Controle de Estoque</title>		    
 	<meta name="description" content="">		    
 	<meta name="viewport" content="width=device-width, initial-scale=1">		
-	<link rel="stylesheet" href="<?php echo BASEURL; ?>/dist/css/bootstrap.min.css">		    
+	<link rel="stylesheet" href="<?php echo BASEURL; ?>css/bootstrap.min.css">		    
 	<style>		        
 	body {		            
 		padding-top: 50px;		            
 		padding-bottom: 20px;		        
 	}		    
 </style>		    
-<link rel="stylesheet" href="<?php echo BASEURL; ?>dist/css/style.css">		    
+<link rel="stylesheet" href="<?php echo BASEURL; ?>css/style.css">		    
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">		
 </head>		
 <body>		
